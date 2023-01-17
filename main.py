4735a2ca9df264149cc1885ec50c1eaab4b99495
samer <khsamer11@gmail.com>
2023-01-17 18:12:35 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import glob
+from cameraCalibration import *
+from poseEstimation import *
+
+if __name__ == '__main__':
+    images = glob.glob('images/*.png')
+    camera = cameraCalibration(images)
+    camera.run()
+
+    pose = poseEstimation(images)
+    pose.run()
